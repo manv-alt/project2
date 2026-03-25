16648428b9bdadf980e3d9d4762c7691000b1610
Manish <manishkarker927@gmail.com>
2026-03-25 11:50:31 +0530

    changes

--- server/Controllers/userctrl.js
+++ server/Controllers/userctrl.js
@@ -9,7 +9,9 @@ import nodemailer from "nodemailer";
 import tempotp from "../Models/TempOtpModel.js";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   family: 4,
   auth: {
     user: process.env.EMAIL_USER,
